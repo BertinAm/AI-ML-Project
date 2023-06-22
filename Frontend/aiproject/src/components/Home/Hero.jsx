ceb3663695060f5d14160c08e5e611347c1645fa
BLADE-HEDA <you@example.com>
2023-06-22 16:47:52 +0100

    hompage in progresst

--- Frontend/aiproject/src/components/Home/Hero.jsx
+++ Frontend/aiproject/src/components/Home/Hero.jsx
@@ -7,8 +7,24 @@ const Hero = () => {
     <p className="hero1">WELCOME  TO </p>
     <h1 className="hero2">Visual-Lingual</h1>
     <section className="card-section">
-        <div className="card1"><Card/> </div>
-        <div className="card1"><Card/> </div>
+        {/* <div className="card1"> */}
+            <Card text="Face detection" 
+            style={{
+                paddingTop:'2em',
+                paddingLeft:'5em',
+                paddingBottom:'2em',
+                paddingRight:'5em'
+             }} />
+             {/* </div> */}
+        {/* <div className="card1"> */}
+            <Card text="Translator"
+              style={{
+                paddingTop:'2em',
+                paddingLeft:'6em',
+                paddingBottom:'2em',
+                paddingRight:'6em'
+             }} /> 
+            {/* </div> */}
     </section>
    </main>
   )
--- Frontend/aiproject/src/components/shared/Card.jsx
+++ Frontend/aiproject/src/components/shared/Card.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faArrowRight } from '@fortawesome/free-solid-svg-icons'
 const Card = (props) => {
   return (
-    <main className="cards">
-        <p className="text">Face Detection</p>
+    <main className="cards" style={props.style} >
+        <p className="text">{props.text}</p>
         <div className="icon">
-
+        <FontAwesomeIcon icon={faArrowRight} className='iconx' />
         </div>
     </main>
   )
