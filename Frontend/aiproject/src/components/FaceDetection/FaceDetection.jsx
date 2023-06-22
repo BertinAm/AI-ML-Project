1d27333a731806ad4624db8267320cfebd377a4f
BLADE-HEDA <you@example.com>
2023-06-22 22:17:04 +0100

    changes done to the facedetector

--- Frontend/aiproject/src/components/FaceDetection/FaceDetection.jsx
+++ Frontend/aiproject/src/components/FaceDetection/FaceDetection.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import './FaceDetection.css'
 const FaceDetection = () => {
   return (
-    <div>FaceDetection</div>
+    <div className='main'>
+        <p className="header">Test Out our Face Detection Algorithm </p>
+        <div className="labels">
+            <div className="labelleft">
+                <p>input</p>
+                <div className="input">
+               <input type="file" className='picinput' />
+                </div>
+            </div>
+            <div className="labelright"></div>
+        </div>
+    </div>
   )
 }
 
