6d302cba8d86fdc8f2523679e6c5f2c475058769
BLADE-HEDA <you@example.com>
2023-06-23 22:24:54 +0100

    changes doen by blanco

--- Frontend/aiproject/src/components/shared/Card.jsx
+++ Frontend/aiproject/src/components/shared/Card.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'react-router-dom';
 
 const Card = (props) => {
   
