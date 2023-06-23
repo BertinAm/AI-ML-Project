9327d4c2f77df917716358d74ebab4b4d898c55b
BLADE-HEDA <you@example.com>
2023-06-23 08:28:05 +0100

    new commit

--- Frontend/aiproject/src/components/shared/Card.jsx
+++ Frontend/aiproject/src/components/shared/Card.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
+
 const Card = (props) => {
+  
   return (
-    <main className="cards" style={props.style} >
+    <main className="cards" style={props.style} > 
         <div className="illustration"><img src={props.src} alt="" className="tllustrate" />    </div>
         <p className="text">{props.text}</p>
         <FontAwesomeIcon icon={faArrowRight} className='iconx' /> 
